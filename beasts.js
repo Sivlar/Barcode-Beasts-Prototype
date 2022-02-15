ac505ec94036a25f9c17d0c52d3fd3f64dfec099
Whitney Facemyer <whitneyfacemyer@gmail.com>
2022-02-15 00:40:35 -0500

    Closes #2, #3

---
+++ beasts.js
@@ -0,0 +1,18 @@
+var DATA = [
+    {
+        "Name": "Graveltusk",
+        "Img": "./img/Graveltusk.png",
+        "HP": 10,
+        "ATK": 10,
+        "DEF": 10,
+        "SPD": 10
+    },
+    {
+        "Name": "Granolafly",
+        "Img": "./img/Granolafly.png",
+        "HP": 10,
+        "ATK": 10,
+        "DEF": 10,
+        "SPD": 10
+    }
+]
