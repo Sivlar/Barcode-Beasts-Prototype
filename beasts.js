549aaa95f59dbc88570d4fec3c481a12a53ed43e
Todd Facemyer <toddfacemyer@ymail.com>
2022-02-15 12:00:36 -0500

    Add all Beast data and any missing images

--- beasts.js
+++ beasts.js
@@ -2,17 +2,97 @@ var DATA = [
     {
         "Name": "Graveltusk",
         "Img": "./img/Graveltusk.png",
-        "HP": 10,
-        "ATK": 10,
-        "DEF": 10,
-        "SPD": 10
+        "HP": "20",
+        "ATK": "5",
+        "DEF": "3",
+        "SPD": "1"
     },
     {
         "Name": "Granolafly",
         "Img": "./img/Granolafly.png",
-        "HP": 10,
-        "ATK": 10,
-        "DEF": 10,
-        "SPD": 10
+        "HP": "10",
+        "ATK": "3",
+        "DEF": "2",
+        "SPD": "3"
+    },
+    {
+        "Name": "EarthTank",
+        "Img": "./img/EarthTank.jpg",
+        "HP": "23",
+        "ATK": "1",
+        "DEF": "5",
+        "SPD": "1"
+    },
+    {
+        "Name": "Wriggler",
+        "Img": "./img/Wriggler.png",
+        "HP": "9",
+        "ATK": "9",
+        "DEF": "1",
+        "SPD": "5"
+    },
+    {
+        "Name": "FireSupport",
+        "Img": "./img/FireSupport.jpg",
+        "HP": "20",
+        "ATK": "2",
+        "DEF": "2",
+        "SPD": "3"
+    },
+    {
+        "Name": "MoltenMenace",
+        "Img": "./img/MoltenMenace.png",
+        "HP": "18",
+        "ATK": "2",
+        "DEF": "3",
+        "SPD": "2"
+    },
+    {
+        "Name": "Illuminator",
+        "Img": "./img/Illuminator.png",
+        "HP": "13",
+        "ATK": "6",
+        "DEF": "2",
+        "SPD": "4"
+    },
+    {
+        "Name": "Optopus",
+        "Img": "./img/Optopus.png",
+        "HP": "16",
+        "ATK": "1",
+        "DEF": "3",
+        "SPD": "3"
+    },
+    {
+        "Name": "Silverfin",
+        "Img": "./img/Silverfin.png",
+        "HP": "22",
+        "ATK": "1",
+        "DEF": "3",
+        "SPD": "3"
+    },
+    {
+        "Name": "Winja",
+        "Img": "./img/Winja.png",
+        "HP": "13",
+        "ATK": "7",
+        "DEF": "2",
+        "SPD": "5"
+    },
+    {
+        "Name": "Bellowsoar",
+        "Img": "./img/Bellowsoar.png",
+        "HP": "14",
+        "ATK": "2",
+        "DEF": "2",
+        "SPD": "4"
+    },
+    {
+        "Name": "BlindingBeetle",
+        "Img": "./img/BlindingBeetle.png",
+        "HP": "20",
+        "ATK": "3",
+        "DEF": "4",
+        "SPD": "3"
     }
 ]
